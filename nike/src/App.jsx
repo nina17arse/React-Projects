c54e03f15ed9055ab857411963397e73bf9473ea
Natnael Wondwoesn <ninatina423@gmail.com>
2023-10-16 18:52:06 +0300

    File Structure

--- nike/src/App.jsx
+++ nike/src/App.jsx
@@ -1,3 +1,27 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import {
+  Hero,
+  CustomerReviews,
+  Subscribe,
+  SpecialOffer,
+  Footer,
+  SuperQuality,
+  Services,
+  PopularProducts,
+} from "./sections";
+import { Nav } from "./components/Nav";
+
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">PopularProducts</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding-x py-1">Services</section>
+    <section className="padding">SpercialOffer</section>
+    <section className="padding">CustomerReciews</section>
+    <section className="padding">Subscribe</section>
+    <section className="padding">Footer</section>
+  </main>
+);
+
+export default App;
---
+++ nike/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  CustomerReviews,
+  Subscribe,
+  Services,
+  SpecialOffers,
+  SuperQuality,
+  Footer,
+};
